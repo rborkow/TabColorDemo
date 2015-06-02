0e22b943d5752c3a4f1e5c830199a8944f9f35e6
rborkow <rborkows@gmail.com>
2015-06-02 16:35:11 -0700

    Initial Commit

---
+++ TabColorDemo/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  TabColorDemo
+//
+//  Created by Robert Borkowski on 6/2/15.
+//  Copyright (c) 2015 Rob Borkowski. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
